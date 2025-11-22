2b2af619fad25e6954790048bafe5d752abc1c67
prasanth-baskar-39 <prasanthbaskar39@gmail.com>
2025-11-22 16:35:50 +0530

    adding CSRF

--- edu_tech_ai_sulution/settings.py
+++ edu_tech_ai_sulution/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://34.42.171.247:8080",
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
